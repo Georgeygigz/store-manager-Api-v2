5b5389df521c0704c31c23b538460565fb4f16f7
GeorgeyGigz <georgeymutti@gmail.com>
2018-10-29 09:50:28 +0300

    [#161538650]Initialize jwt manager

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ Register Blueprints
 '''
 from flask import Flask, Blueprint
 from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from datetime import timedelta
 
 
 # local imports
@@ -20,6 +22,9 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_configuration['development'])
     app.register_blueprint(blueprint)
+    app.config['JWT_SECRET_KEY'] = "vch37fhdser20rdbsk"
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=72)
+    jwt = JWTManager(app)
     create_table()
     app_api.add_resource(ViewProducts, '/products')
     app_api.add_resource(ViewSingleProduct, '/products/<int:product_id>')
